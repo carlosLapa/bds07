b49e9e3b10fdf6ff58b67bf387511a681aef2803
carlosLapa <carlosferreiralapa@gmail.com>
2022-08-19 10:37:37 +0100

    Navbar styling

--- bds07/src/App.tsx
+++ bds07/src/App.tsx
@@ -1,10 +1,10 @@
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
+
 function App() {
-  return (
-    <h1> Hello World!</h1>
-  );
+  return <Navbar />;
 }
 
 export default App;
