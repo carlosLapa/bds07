7177be3e472d34a2b1c29cc62876a03d9ec535af
carlosLapa <carlosferreiralapa@gmail.com>
2022-09-06 18:13:29 +0100

    Terminado

--- bds07/src/pages/Catalog/index.tsx
+++ bds07/src/pages/Catalog/index.tsx
@@ -4,19 +4,31 @@ const Catalog = () => {
   return (
     <div className="container my-4">
       <div className="row">
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <CarCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <CarCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <CarCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <CarCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <CarCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <CarCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <CarCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <CarCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <CarCard />
         </div>
       </div>
